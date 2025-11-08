4cfdacb9b18c9d0fa36c77101b1ceaed0878fea0
alaadev <alaadindev@gmail.com>
2025-11-08 13:29:16 +0200

    added js

---
+++ script.js
@@ -0,0 +1,36 @@
+let textfield = document.getElementById("input")
+let btn = document.getElementById("add")
+let tasksdiv = document.getElementById("tasks")
+let tasks = []
+
+btn.addEventListener("click", addtask)
+textfield.addEventListener("keydown", usertyped)
+
+function addtask(){
+    let text = textfield.value
+    if (tasks.includes(text) || text == ""){
+        return
+    }
+    tasks.push(text)
+    textfield.value = ""
+    console.log(text)
+    let task = document.createElement("section")
+    let span = document.createElement("span")
+    span.textContent = text
+    task.appendChild(span)
+    task.className = "task"
+    task.addEventListener("click", deletetask)
+    tasksdiv.appendChild(task)
+}
+function deletetask(e){
+    e.currentTarget.classList.add("grayed")
+}
+function usertyped(){
+    if (e.key == "Enter"){
+        btn.click()
+        return
+    }
+    if (tasks.includes(textfield.value)){
+        
+    }
+}
